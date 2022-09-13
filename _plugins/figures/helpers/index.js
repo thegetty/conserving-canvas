07d636c2162c1ff13c6bda175d7dd545ccd56d05
Greg Albers <galbers@getty.edu>
2022-09-13 11:01:50 -0700

    Initial commit on forthcoming page

--- _plugins/figures/helpers/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  getPrintImage: require('./get-print-image'),
-  isCanvas: require('./is-canvas'),
-  isImageService: require('./is-image-service')
-}
--- _plugins/figures/helpers/is-canvas.js
+++
@@ -1,3 +0,0 @@
-module.exports = ({ annotations, id, canvasId, manifestId, iiifContent }) => {
-  return (!!canvasId && !!manifestId) || !!iiifContent || !!annotations
-}
